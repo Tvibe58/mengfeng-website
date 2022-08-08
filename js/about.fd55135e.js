2386b5612a22fb822085ed2767976b9e10dc5135
Tvibe <tvibe58@163.com>
2022-08-08 17:04:14 +0800

    Updates

--- js/about.fd55135e.js
+++ js/about.fd55135e.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkmengfeng_website"]=self["webpackChunkmengfeng_website"]||[]).push([[443],{7381:function(e,n,t){t.r(n),t.d(n,{default:function(){return b}});var s=t(3396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function i(e,n){return(0,s.wg)(),(0,s.iD)("div",u,r)}var c=t(89);const f={},o=(0,c.Z)(f,[["render",i]]);var b=o}}]);
-//# sourceMappingURL=about.1a3b53e2.js.map
+//# sourceMappingURL=about.fd55135e.js.map
