aad4702deef97d9c104d75693ac2447c95dae7d6
Tvibe <tvibe58@163.com>
2022-08-09 14:00:35 +0800

    引入ElementPlus&@element-plus/icons-vue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(store)
+  .use(router)
+  .use(ElementPlus, {
+    locale: zhCn
+  })
+  .mount('#app')
